d559b35a13bdd3f8b4c8bb5f088c24671d134d96
iamrochi <rmercado373@alumno.unlam.edu.ar>
2024-11-04 17:29:56 -0300

    Analytics: seguimiento de la pantalla home y el boton refresh

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/screens/HomeScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/screens/HomeScreen.kt
@@ -3,17 +3,29 @@ package ar.edu.unlam.mobile.scaffold.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import ar.edu.unlam.mobile.scaffold.domain.kitty.models.Kitty
 import ar.edu.unlam.mobile.scaffold.ui.components.PhotoCard
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.analytics
+import com.google.firebase.analytics.logEvent
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier, viewModel: HomeViewModel = hiltViewModel()) {
     // La información que obtenemos desde el view model la consumimos a través de un estado de "tres vías".
     // Loading, Success y Error. Esto nos permite mostrar un estado de carga, un estado de éxito y un mensaje de error.
+    val firebaseAnalytics = Firebase.analytics
+
+    // Registra un evento de visita a la pantalla
+    LaunchedEffect(Unit) {
+        firebaseAnalytics.logEvent("home_screen_viewed") {
+            param("screen_name", "HomeScreen")
+        }
+    }
     val uiState: HomeUIState by viewModel.uiState.collectAsState()
 
     when (val kittyState = uiState.kittyState) {
